548bbe1bf1fb6999bda22269fa59adbcaa80464c
qglove <2545671842@qq.com>
2024-02-02 16:02:30 +0800

    feat

--- src/components/Intl/index.js
+++ src/components/Intl/index.js
@@ -43,7 +43,7 @@ const Intl = {
       console.error('Intl组件：init传入的国际化数据不是数组');
     }
 
-    zhList?.concat(locales?.zh_CN?.zh_CN_List)?.forEach?.((value, i) => {
+    locales?.zh_CN?.zh_CN_List?.concat(zhList)?.forEach?.((value, i) => {
       const key = `t_${i}`;
 
       intlKeyList.push(key);
